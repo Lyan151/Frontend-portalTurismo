1d5a8d3f4cb675dba0fb56d6fdb91d30eba99ff6
Lyan151 <lyanangell028@gmail.com>
2025-08-01 14:34:49 -0300

    update

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,7 +9,7 @@ const LoginForm = () =>{
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const response = await axios.post("http://localhost:5000/api/auth/login", {
+        const response = await axios.post("https://backend-poratlturismo-cm8i.onrender.com/api/auth/login", {
             email,
             password: senha
         })
